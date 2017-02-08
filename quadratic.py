ed8a63f76c86945f1ed00fd08ea25892178de4d1
aixiangyu <jupiter.aixiangyu@qq.com>
2017-02-08 14:46:52 +0800

    add quadratic function

---
+++ quadratic.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+#一元二次方程
+
+import math
+
+def quadratic(a, b, c):
+
+	if not isinstance(a, (int, float)):
+		raise TypeError('a is bad operand type')
+
+	if not isinstance(b, (int, float)):
+		raise TypeError('b is bad operand type')
+
+	if not isinstance(c, (int, float)):
+		raise TypeError('c is bad operand type')
+
+	if  a == 0:
+		print('a can\'t equal zero')
+		return 
+
+	delta = b * b - 4 * a * c
+
+	if delta < 0 :
+		print('no result!')
+
+	x1 = (-b + math.sqrt(delta)) / (2 * a)
+
+	x2 = (-b - math.sqrt(delta)) / (2 * a)
+	
+	return x1, x2
+
+
+a = 2
+b = 3
+c = 1
+print(quadratic(a, b, c))
+
+
+
+
+
+
+
+
+
+
